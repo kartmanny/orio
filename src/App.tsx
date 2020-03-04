5ba35b55cca516c8787ec52902afced5f08d1526
Karthik Manickam <karthik.omanickam@gmail.com>
2020-03-04 10:14:09 -0800

    Initial typescript setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import 'assets/styles/app.scss';
+
+import Text from 'components/Text';
+import Navbar from 'components/Navbar';
+
+const ROUTES = [
+  {
+    name: 'home',
+    component: () => <Text type="heading1">Path /</Text>,
+    path: '/',
+    exact: true
+  },
+  {
+    name: 'discover',
+    component: () => <Text type="heading1">Path discover</Text>,
+    path: '/discover',
+    exact: true
+  },
+  {
+    name: 'favorites',
+    component: () => <Text type="heading1">Path favorites</Text>,
+    path: '/favorites',
+    exact: true
+  }
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Navbar routes={ROUTES} />
+      <Switch>
+        {ROUTES.map(({ path, component, exact, name }) => (
+          <Route
+            path={path}
+            component={component}
+            exact={exact}
+            key={`route-${name}`}
+          />
+        ))}
+      </Switch>
+    </BrowserRouter>
   );
 }
 
