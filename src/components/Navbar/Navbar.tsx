49f30854785be5e01c3781b59624c5405219ef30
Karthik Manickam <karthik.omanickam@gmail.com>
2020-03-04 10:35:41 -0800

    Added function Navbar component and styles

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,4 +1,9 @@
 import React from 'react';
+import styled from 'styled-components';
+import { NavLink } from 'react-router-dom';
+
+import Text from 'components/Text';
+import styles from 'components/Navbar/navbar.module.scss';
 
 type Route = {
   name: string;
@@ -11,13 +16,28 @@ interface INavbarProps {
   routes: Array<Route>;
 }
 
+const Nav = styled.nav`
+  padding: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
 function Navbar({ routes }: INavbarProps) {
   return (
-    <nav>
-      {routes.map(route => (
-        <div>{route.name}</div>
+    <Nav>
+      {routes.map(({ path, name, exact = true }, index) => (
+        <NavLink
+          to={path}
+          className={styles.navLink}
+          activeClassName={styles.active}
+          key={`NavLink-${name}-${index}`}
+          exact={exact}
+        >
+          <Text type="large">{name}</Text>
+        </NavLink>
       ))}
-    </nav>
+    </Nav>
   );
 }
 
