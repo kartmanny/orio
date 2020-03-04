8d878def620eb1be4bb8ba92564fb5bb3741d932
Karthik Manickam <karthik.omanickam@gmail.com>
2020-03-04 10:14:51 -0800

    Added Text component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import cx from 'classnames';
+import styles from 'components/Text/text.module.scss';
+
+interface ITextProps {
+  type: 'heading1' | 'heading2' | 'heading3' | 'large' | 'regular' | 'small';
+  children: React.ReactNode;
+  color?: 'heading' | 'body';
+  weight?: 'heading' | 'body';
+}
+
+const Text = ({ type, children, color, weight }: ITextProps) => {
+  return type.match(/^heading/) ? (
+    <h1
+      className={cx(
+        styles[type],
+        color && styles[`color-${color}`],
+        weight && styles[`weight-${weight}`]
+      )}
+    >
+      {children}
+    </h1>
+  ) : (
+    <span
+      className={cx(
+        styles[type],
+        color && styles[`color-${color}`],
+        weight && styles[`weight-${weight}`]
+      )}
+    >
+      {children}
+    </span>
+  );
+};
+
+export default Text;
